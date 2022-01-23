c019da630c2f18f2875c6873924fa4f1531e742b
tracy <tracywang1997@outlook.com>
2022-01-23 16:23:57 +0800

    解决ui bug

--- src/views/Mirror.vue
+++ src/views/Mirror.vue
@@ -21,13 +21,14 @@ import { onMounted, ref } from 'vue'
 import { createViewer, Marker,Measure, Mirror} from '../../libs';
 import SwitchMap from '../components/SwitchMap.vue';
 import DrawTool from '../components/DrawTool.vue';
+import { Mark } from 'element-plus';
 
 const container_master = "container_master";
 const container_slave = "container_slave";
 
 let viewer_master = ref<Viewer>();
 let viewer_slave = ref<Viewer>();
-let marker = ref<Measure>();
+let marker = ref<Marker>();
 
 onMounted(() => {
     viewer_master.value = createViewer(container_master);
@@ -36,7 +37,7 @@ onMounted(() => {
     new Mirror(viewer_master.value,viewer_slave.value)
 
     //marker.value = new Marker(viewer_master.value,entity=>viewer_slave.value?.entities.add(entity));
-    marker.value = new Measure(viewer_master.value);
+    marker.value = new Marker(viewer_master.value);
     marker.value.start("Polygon");
 })
 
